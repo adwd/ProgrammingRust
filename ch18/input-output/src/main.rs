6fcda346eac89acd37133b0d72d7ade527b05b66
adwd <ecdt.mn@gmail.com>
2022-02-10 21:30:47 +0900

    ch18

--- ch18/input-output/src/main.rs
+++ ch18/input-output/src/main.rs
@@ -71,5 +71,116 @@ fn main() {
             }
             Ok(())
         }
+
+        fn grep2<R>(target: &str, reader: R) -> io::Result<()>
+        where
+            R: io::BufRead,
+        {
+            for line_result in reader.lines() {
+                let line = line_result?;
+                if line.contains(target) {
+                    println!("{line}");
+                }
+            }
+            Ok(())
+        }
+
+        use std::error::Error;
+        use std::fs::File;
+        use std::io::{BufReader, BufWriter};
+        use std::path::PathBuf;
+
+        fn grep3<R>(target: &str, reader: R) -> io::Result<()>
+        where
+            R: io::BufRead,
+        {
+            for line_result in reader.lines() {
+                let line = line_result?;
+                if line.contains(target) {
+                    println!("{}", line);
+                }
+            }
+            Ok(())
+        }
+
+        fn grep_main() -> Result<(), Box<dyn Error>> {
+            let mut args = std::env::args().skip(1);
+            let target = match args.next() {
+                Some(s) => s,
+                None => return Err("usage: grep PATTERN FILE...".into()),
+            };
+            let files = args.map(PathBuf::from).collect::<Vec<PathBuf>>();
+
+            if files.is_empty() {
+                let stdin = io::stdin();
+                grep3(&target, stdin.lock())?;
+            } else {
+                for file in files {
+                    let f = File::open(file)?;
+                    grep3(&target, BufReader::new(f))?;
+                }
+            }
+
+            Ok(())
+        }
+
+        let f = File::open("test.txt").unwrap();
+        let reader = BufReader::new(f);
+        let lines = reader.lines();
+
+        // Vec<io::Result<String>>じゃなくResult<Vec<String>>をcollectできる
+        // let z = lines.collect::<Vec<io::Result<String>>>();
+        let x = lines.collect::<io::Result<Vec<String>>>();
+
+        // writer
+        // writer.write(&buf), write_all(&buf), flush(): バッファされたデータを書き出す
+
+        // file
+        // File::open(filename), File::create(filename)
+        // OpenOptions
+
+        // Seek
+
+        // 他のreader, writer
+        // io::stdin(), io::stdout(), io::stderr()
+        // Vec<u8>
+        // Cursor::new(buf) bufから読み出すバッファ付きreaderであるCursorを生成すsる
+        // std::net::TcpStream
+        // std::process::Command
+        // io::sink(), io::empty(), io::repeat(byte)
+    }
+
+    // ファイルとディレクトリ
+    {
+        // OsStr, Path
+        // ファイル名に有効ではないUnicodeがありうるのでそれに対処するのがstd::ffi::OsStr, OsString
+        // Stringはヒープ上に確保されたstrを所有する
+        // std::ffi::OsStringはヒープ上に確保されたOsStrを所有する
+        // std::path::PathBufはヒープ上に確保されたPathを所有する
+
+        // Path::new(str)
+        // .parent(), file_name(), is_absolute(), is_relative(), join(path2), components(), ancestors(), to_str(), to_string_lossy(), display()
+        // create_dir, remove_dir, remove_file, copy, rename, hard_link, ...
+
+        // プラットフォーム固有
+    }
+
+    // ネットワークプログラム
+    {
+        // std::netで低レベルなネットワークをかける
+
+        use std::error::Error;
+        use std::io;
+
+        fn http_get_main(url: &str) -> Result<(), Box<dyn Error>> {
+            let mut response = reqwest::blocking::get(url)?;
+            if !response.status().is_success() {
+                return Err(format!("{}", response.status()).into());
+            }
+
+            let stdout = io::stdout();
+            io::copy(&mut response, &mut stdout.lock())?;
+            Ok(())
+        }
     }
 }
