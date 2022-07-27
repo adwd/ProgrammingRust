a4f283778c7ba7c85d0f432c9f4cfc63daa77bef
adwd <adwd@users.noreply.github.com>
2022-07-27 17:53:31 +0900

    update actix-gcd

--- ch02/actix-gcd/src/main.rs
+++ ch02/actix-gcd/src/main.rs
@@ -1,32 +1,18 @@
-use actix_web::{web, App, HttpResponse, HttpServer};
+use actix_web::{get, post, web, App, HttpResponse, HttpServer, Responder};
 use serde::Deserialize;
 
-fn main() {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/", web::get().to(get_index))
-            .route("/gcd", web::post().to(post_gcd))
-    });
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let server = HttpServer::new(|| App::new().service(get_index).service(post_gcd));
 
     println!("Serving on http://localhost:3000");
-    server
-        .bind("127.0.0.1:3000")
-        .expect("error binding server to address")
-        .run()
-        .expect("error running server");
+    server.bind("127.0.0.1:3000")?.run().await
 }
 
-fn get_index() -> HttpResponse {
-    HttpResponse::Ok().content_type("text/html").body(
-        r###"
-            <title>GCD Calculator</title>
-            <form action="/gcd" method="post">
-            <input type="text" name="n" />
-            <input type="text" name="m" />
-            <button type="submit">Comupte GCD</button>
-            </form>
-        "###,
-    )
+#[get("/")]
+async fn get_index() -> impl Responder {
+    let content = include_str!("index.html");
+    HttpResponse::Ok().content_type("text/html").body(content)
 }
 
 #[derive(Deserialize)]
@@ -35,7 +21,8 @@ struct GcdParameters {
     m: u64,
 }
 
-fn post_gcd(form: web::Form<GcdParameters>) -> HttpResponse {
+#[post{"/gcd"}]
+async fn post_gcd(form: web::Form<GcdParameters>) -> impl Responder {
     if form.n == 0 || form.m == 0 {
         return HttpResponse::BadRequest()
             .content_type("text/html")
@@ -58,11 +45,9 @@ fn gcd(mut n: u64, mut m: u64) -> u64 {
 
     while m != 0 {
         if m < n {
-            let t = m;
-            m = n;
-            n = t;
+            std::mem::swap(&mut m, &mut n);
         }
-        m = m % n;
+        m %= n;
     }
     n
 }
